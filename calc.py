e5d600f3f86262bc831083c31855e044d372b3e4
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-08-20 09:36:09 +0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,22 @@
+#first Calculator
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the First Number: "))
+opr = input("Add, Subtract, Multiply, Divide? Please type the complete word")
+res = 0
+if (int(num1) and int(num2) == 0):
+    print("Invalid Inputs")
+else:
+    if opr == "Add":
+        res = num1 + num2
+        print(res)
+    elif opr == "Subtract":
+        res = num1 - num2
+        print(res)
+    elif opr == "Multiply":
+        res = num1 * num2
+        print(res)
+    elif opr == "Divide":
+        res = num1 / num2
+        print(res)
+    else:
+        print("Invalid Operator")
