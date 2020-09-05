fe5146977609ce26ff2934f056f297a08f28ab00
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-09-05 10:50:48 +0500

    Update and rename html_closetag to html_closetag.py

--- html_closetag.py
+++ html_closetag.py
@@ -1,7 +1,9 @@
 for string in html_tags:
+    # Complete the regex and find if it matches a closed HTML tags
     match_tag =  re.match(r"<(\w+)>.*?</\1>", string)
  
     if match_tag:
+        # If it matches print the first group capture
         print("Your tag {} is closed".format(match_tag.group(1))) 
     else:
         # If it doesn't match capture only the tag 
