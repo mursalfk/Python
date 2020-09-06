d0f778adcf8818b37b0b1dca9d1abc0de84b368a
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-09-06 17:43:01 +0500

    Update test_row_to_list.py

--- Unit Tests/test_row_to_list.py
+++ Unit Tests/test_row_to_list.py
@@ -4,3 +4,7 @@ def test_for_clean_row():
   # assert Statement only takes a boolean expression
   # If the boolean is True, it passes, if False, it failes and shows an error
   assert row_to_list("2,081\t314,942\n") == \ ["2,081","314,942"]
+
+ 
+def test_for_missing_area():
+  assert row_to_list("\293,410\n") is None
