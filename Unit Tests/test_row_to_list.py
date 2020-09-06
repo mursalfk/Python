490907d5f3873641af08098df8558feaea7dd0ee
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-09-06 17:41:46 +0500

    Update test_row_to_list.py

--- Unit Tests/test_row_to_list.py
+++ Unit Tests/test_row_to_list.py
@@ -1,4 +1,6 @@
 import pytest
 import row_to_list
 def test_for_clean_row():
+  # assert Statement only takes a boolean expression
+  # If the boolean is True, it passes, if False, it failes and shows an error
   assert row_to_list("2,081\t314,942\n") == \ ["2,081","314,942"]
